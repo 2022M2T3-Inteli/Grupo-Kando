bd8423ccf54659c0083fa063288d6ad107882cd9
gabrielpascoli <gabriel.pascoli@sou.inteli.edu.br>
2022-05-11 15:50:46 -0300

    adicionado modal /imagens do menu

---
+++ src/js/cadastro_projeto.js
@@ -0,0 +1,10 @@
+function abrir(){
+    //animação para abrir o modal
+     var modal = document.querySelector('.modal');
+     modal.style.display = 'block';
+ }
+ function fechar(){
+     //animação para fechar o modal
+     var modal = document.querySelector('.modal');
+     modal.style.display = 'none';
+ }
