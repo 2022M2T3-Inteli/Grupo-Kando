aba95dfcf70fa1fcf04520985d04dd01e49c0231
mateusalm <mateus.almeida@sou.inteli.edu.br>
2022-05-22 17:45:56 -0300

    alguns comentários no codigo
    comentários adicionados nas funções da tela de funcionários e de projetos

--- src/js/project-add-employee-role.js
+++ src/js/project-add-employee-role.js
@@ -2,6 +2,7 @@ var rolesAdded = document.querySelector('#roles-added')
 var newEmployeeRole = document.querySelector('#new-employee-role')
 var newEmployeeRoleName = newEmployeeRole.options[select.selectedIndex].text
 
+// function que adiciona uma função na 
 function newRoleName() {
   document.querySelector('#role-selected01').text = newEmployeeRoleName
 }
